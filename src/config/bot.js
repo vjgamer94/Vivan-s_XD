33b021dccfb47fbc817007aa1b8d05bd7b15a750
vjgamer94 <140325767+vjgamer94@users.noreply.github.com>
2026-08-10 13:59:19 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: "MAKING DAY FOR VIVAN", // required by Discord API, not shown in the client
+        state: "MADE FOR VIVAN ZONE",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
